bbd794eba4134a9e580bbb03d3db758a17637b1c
Volodymyr Denysov <vdenysov@appolica.com>
2025-11-28 21:49:50 +0200

    Task
    Added: setup

---
+++ MultibankTask/MultibankTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultibankTaskApp.swift
+//  MultibankTask
+//
+//  Created by Volodymyr Denysov on 28.11.25.
+//
+
+import SwiftUI
+
+@main
+struct MultibankTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
